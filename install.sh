fb99dd10472e4bd402fd71c77b2e36668e023e7b
Andrew Cutler <macropin@gmail.com>
2013-10-14 16:53:40 +1100

    Missed rename of libjpeg to libjpeg-turbo

--- install.sh
+++ install.sh
@@ -49,7 +49,7 @@ function _install_python_requirements {
     fi
     
     # Install gcc & development libs so we can compile PIL later (FIXME: PIL Required?)
-    if ! rpm -q gcc freetype freetype-devel libpng libjpeg libpng-devel libjpeg-turbo-devel python-devel 1> /dev/null; then
+    if ! rpm -q gcc freetype freetype-devel libpng libjpeg-turbo libpng-devel libjpeg-turbo-devel python-devel 1> /dev/null; then
         yum -y -q install gcc freetype freetype-devel libpng libjpeg libpng-devel libjpeg-devel python-devel
     fi
     
